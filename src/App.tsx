f9c01bf1686569d095c1574c82061bf93b351683
DayanDec <dayan.decamp@etu.univ-orleans.fr>
2025-01-28 07:21:21 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { Home } from './pages/Home';
 import { Products } from './pages/Products';
@@ -17,6 +17,8 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/book" element={<Book />} />
+          {/* Redirection pour toutes les routes non définies */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
